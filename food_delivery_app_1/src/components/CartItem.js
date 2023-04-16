065f0343713132fcfeadbe08564da9166fa359f6
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-04-16 17:32:20 +0600

    updated cart intem functionality

--- food_delivery_app_1/src/components/CartItem.js
+++ food_delivery_app_1/src/components/CartItem.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 import { AddRounded, RemoveRounded } from "@mui/icons-material";
 import React, { useEffect, useState } from "react";
 import { useStateValue } from "./StateProvider";
@@ -5,64 +6,64 @@ import { useStateValue } from "./StateProvider";
 let cartItems = [];
 
 function CartItem({ name, imgSrc, price, itemId }) {
-    // eslint-disable-next-line no-unused-vars
-    const [qty, setQty] = useState(1);
+  // eslint-disable-next-line no-unused-vars
+  const [qty, setQty] = useState(1);
 
-    // eslint-disable-next-line no-unused-vars
-    const [{ cart }, dispatch] = useStateValue();
-    const [itemPrice, setItemPrice] = useState(parseInt(qty) * parseFloat(price));
+  // eslint-disable-next-line no-unused-vars
+  const [{ cart }, dispatch] = useStateValue();
+  const [itemPrice, setItemPrice] = useState(parseInt(qty) * parseFloat(price));
 
-    useEffect(() => {
-        cartItems = cart;
-        setItemPrice(parseInt(qty) * parseFloat(price));
-    }, [qty]);
+  useEffect(() => {
+    cartItems = cart;
+    setItemPrice(parseInt(qty) * parseFloat(price));
+  }, [qty]);
 
-    const updateQuantity = (action, id) => {
-        if (action === "add") {
-            setQty(qty + 1);
-        } else {
-            if (qty === 1) {
-                cartItems.pop(id);
-                dispatch({
-                    type: "SET_CART",
-                    cart: cartItems
-                });
-            }
-            setQty(qty - 1);
-        }
-    };
-    return (
-        <div className="cartItem">
-            <div className="imgBox">
+  const updateQuantity = (action, id) => {
+    if (action === "add") {
+      setQty(qty + 1);
+    } else {
+      if (qty === 1) {
+        const updatedCart = cartItems.filter((item) => item.id !== id);
+        dispatch({
+          type: "SET_CART",
+          cart: updatedCart,
+        });
+      }
 
-                <img src={imgSrc} alt="" />
+      setQty(qty - 1);
+    }
+  };
+  return (
+    <div className="cartItem">
+      <div className="imgBox">
+        <img src={imgSrc} alt="" />
+      </div>
 
-            </div>
+      <div className="itemSection">
+        <h2 className="itemName">{name}</h2>
 
-            <div className="itemSection">
+        <div className="itemQuantity">
+          <span>X {qty}</span>
 
-                <h2 className="itemName">{name}</h2>
-
-                <div className="itemQuantity">
-
-                    <span>X {qty}</span>
-
-                    <div className="quantity">
-                        <RemoveRounded className="itemRemove" onClick={() => updateQuantity("remove", itemId)} />
-                        <AddRounded className="itemAdd" onClick={() => updateQuantity("add", itemId)} />
-                    </div>
-
-                </div>
-
-            </div>
-
-            <p className="itemPrice">
-                <span className="dollarSign">$ </span>
-                <span className="itemPriceValue">{itemPrice}</span>
-            </p>
-
+          <div className="quantity">
+            <RemoveRounded
+              className="itemRemove"
+              onClick={() => updateQuantity("remove", itemId)}
+            />
+            <AddRounded
+              className="itemAdd"
+              onClick={() => updateQuantity("add", itemId)}
+            />
+          </div>
         </div>
-    );
+      </div>
+
+      <p className="itemPrice">
+        <span className="dollarSign">$ </span>
+        <span className="itemPriceValue">{itemPrice}</span>
+      </p>
+    </div>
+  );
 }
 
 export default CartItem;
